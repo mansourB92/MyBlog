80cbfe9010d048502da0d984a1d36db091eb057c
mansourB92 <mansour.abcz@gmail.com>
2023-01-13 11:43:54 +0330

    Update form_blog.py

--- Blog/form_blog.py
+++ Blog/form_blog.py
@@ -8,15 +8,6 @@ class RegForm(FlaskForm):
     email = StringField("email", validators=[DataRequired(), Email()])
     password = PasswordField("password", validators=[DataRequired()])
 
-    #def validate_username(self, username):
-    #    user = User.query.filter_by(user_name=username.data).first()
-    #    if user:
-    #        raise ValidationError('This username already exists')
-
-    #def validate_email(self, email):
-    #    user = User.query.filter_by(email=email.data).first()
-    #    if user:
-    #        raise ValidationError('This email already exists')
 
 class LogForm(FlaskForm):
     email = StringField("email", validators=[DataRequired(), Email()])
