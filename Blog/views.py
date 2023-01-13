edf2fd5674e50fb21b55cd6460c7f6c3e4f5751e
mansourB92 <mansour.abcz@gmail.com>
2023-01-13 11:41:10 +0330

    Update views.py

--- Blog/views.py
+++ Blog/views.py
@@ -3,7 +3,7 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, request
 from Blog import app, db, bcrypt
 from Blog.database import *
 from Blog.form_blog import *
@@ -42,13 +42,17 @@ def about():
 def Reg():
     """Renders the about page."""
     RForm = RegForm()
-    if RForm.validate_on_submit():
+    ch_user = User.query.filter_by(user_name=RForm.username.data).first()
+    ch_email = User.query.filter_by(email=RForm.email.data).first()
+    if RForm.validate_on_submit() and not(ch_user) and not(ch_email):
         pw_hash = bcrypt.generate_password_hash(RForm.password.data).decode('utf-8')
         user = User(user_name=RForm.username.data, pass_word=pw_hash, email=RForm.email.data)
         db.session.add(user)
         db.session.commit()
         flash(f"Welcome {RForm.username.data}, Your registered successfully", "success")
         return redirect(url_for('home'))
+    elif request.method == "POST":
+        flash("This Username or Email already exists", "warning")
     return render_template(
         'Reg.html',
         title='Register',
