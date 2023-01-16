777860cd9ef9b51a61d2b59cb12946ce31f3a8cc
mansourB92 <mansour.abcz@gmail.com>
2023-01-16 20:21:24 +0330

    Update form_blog.py

--- Blog/form_blog.py
+++ Blog/form_blog.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
+from wtforms import StringField, PasswordField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
 from Blog.database import *
 
@@ -13,3 +13,11 @@ class LogForm(FlaskForm):
     username = StringField("username", validators=[DataRequired()])
     password = PasswordField("password", validators=[DataRequired()])
     remember = BooleanField("remember me")
+
+class UpdateProfile(FlaskForm):
+    username = StringField("username", validators=[DataRequired(), Length(min=4, max=25, message="username must be between 4 and 25 characters")])
+    email = StringField("email", validators=[DataRequired(), Email()])
+
+class PostForm(FlaskForm):
+    title = StringField("Title", validators=[DataRequired()])
+    content = TextAreaField("Content", validators=[DataRequired()])
